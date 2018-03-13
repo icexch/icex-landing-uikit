6ce90c244e7501bcd1b0c0c0e10aee88dd23c8d3
Sergey <memonanofsa@gmail.com>
2018-03-13 18:00:59 +0500

    ICEX-737 #comment: fix header error

--- test/specs/Header.spec.js
+++ test/specs/Header.spec.js
@@ -1,30 +1,18 @@
 import LayoutHeader from 'src/components/LayoutHeader/LayoutHeader.vue'
 import { createVM } from '../helpers/utils.js'
 
-// import { mount } from '@vue/test-utils'
-// import { expect } from 'chai'
-
-import { locales } from 'src/store/glossary'
+import {locales} from 'src/store/glossary'
 
 describe('LayoutHeader.vue', function () {
-  it('should render LayoutHeader components', () => {
+  it('should render LayoutHeader components', function () {
     const vm = createVM(this,
       `<LayoutHeader 
-        
+        headerLogo="../../assets/img/logo.svg" 
         locales="${locales}"
       >
 
       </LayoutHeader>`,
       { components: { LayoutHeader }})
     vm.$el.querySelector('.header__container h1').textContent.should.eql('hello header')
-
-    // const wrapper = mount(LayoutHeader, {
-    //   propsData: {
-    //     headerLogo: "../../assets/img/logo.svg",
-    //     locales,
-    //   }
-    // })
-    // expect(wrapper.props('locales', locales)).toBe(true)
   })
 })
-
