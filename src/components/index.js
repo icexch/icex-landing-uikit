d54e2828bd59d5612170129fc5497e085d69659c
Sergey <memonanofsa@gmail.com>
2018-03-16 21:54:28 +0500

    ICEX-737 #comment: fix bugs and add arrow

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,17 @@
-import uiFooter from './uiFooter/uiFooter.vue'
-import uiForm from './uiForm/uiForm.vue'
-import uiHeader from './uiHeader/uiHeader.vue'
-import uiLink from './uiLink/uiLink.vue'
-import uiSection from './uiSection/uiSection.vue'
-import uiSlider from './uiSlider/uiSlider.vue'
-import uiSocials from './uiSocials/uiSocials.vue'
+// import uiFooter from './uiFooter/uiFooter.vue'
+// import uiForm from './uiForm/uiForm.vue'
+// import uiHeader from './uiHeader/uiHeader.vue'
+// import uiLink from './uiLink/uiLink.vue'
+// import uiSection from './uiSection/uiSection.vue'
+// import uiSlider from './uiSlider/uiSlider.vue'
+// import uiSocials from './uiSocials/uiSocials.vue'
 
-export default {
-  uiFooter,
-  uiForm,
-  uiHeader,
-  uiLink,
-  uiSection,
-  uiSlider,
-  uiSocials,
-}
+// export default {
+//   uiFooter,
+//   uiForm,
+//   uiHeader,
+//   uiLink,
+//   uiSection,
+//   uiSlider,
+//   uiSocials,
+// }
