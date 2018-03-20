83df495cd5b3e8b7fa2734eba6e2cdd1e03aee88
Sergey <memonanofsa@gmail.com>
2018-03-20 14:53:39 +0500

    ICEX-737 #comment remove uiSlider

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import uiForm from './components/uiForm';
 import uiHeader from './components/uiHeader';
 import uiLink from './components/uiLink';
 import uiSection from './components/uiSection';
-import uiSlider from './components/uiSlider';
+// import uiSlider from './components/uiSlider';
 import uiSocials from './components/uiSocials';
 
 const uiKit = {
@@ -14,7 +14,7 @@ const uiKit = {
     Vue.component('uiHeader', uiHeader);
     Vue.component('uiLink', uiLink);
     Vue.component('uiSection', uiSection);
-    Vue.component('uiSlider', uiSlider);
+    // Vue.component('uiSlider', uiSlider);
     Vue.component('uiSocials', uiSocials);
   }
 };
